d00eb8bde4f0118b0492c08e707f1ff13831c95e
Pedro Veras <veraspedro1608@gmail.com>
2023-02-25 16:35:50 -0300

    criando controller de usuarios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+//routes => rotas para criar um usuario
+//controllers -> controlar a chamada, tratar os erros
+//repositories -> operações no banco de dados
+
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
