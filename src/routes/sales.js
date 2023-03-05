a5e177a169649d7815428bc9e838748ce80ac13a
Pedro Veras <veraspedro1608@gmail.com>
2023-03-05 16:29:50 -0300

    add verifyToken nas rotas

--- src/routes/sales.js
+++ src/routes/sales.js
@@ -8,9 +8,9 @@ const {
 } = require("../controllers/sales");
 
 exports.salesRoutes = app => {
-    app.post("/sales", /* verifyToken, */ create);
-    app.get("/sales", /* verifyToken, */ get);
-    app.get("/sales/:id", /* verifyToken, */ getId);
-    app.put("/sales/:id", /* verifyToken, */ update);
-    app.delete("/sales/:id", /* verifyToken, */ remove);
+    app.post("/sales", verifyToken, create);
+    app.get("/sales", verifyToken, get);
+    app.get("/sales/:id", verifyToken, getId);
+    app.put("/sales/:id", verifyToken, update);
+    app.delete("/sales/:id", verifyToken, remove);
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,8 +3,8 @@ const {verifyToken} = require("../middlewares/auth")
 
 exports.userRoutes = (app) => {
   app.post("/user", create);
-  app.get("/user", /* verifyToken, */ get);
-  app.get("/user/:id", /* verifyToken, */ getId);
-  app.put("/user/:id", /* verifyToken, */ update);
-  app.delete("/user/:id", /* verifyToken, */ remove);  
+  app.get("/user", verifyToken, get);
+  app.get("/user/:id", verifyToken, getId);
+  app.put("/user/:id", verifyToken, update);
+  app.delete("/user/:id", verifyToken, remove);  
 };
